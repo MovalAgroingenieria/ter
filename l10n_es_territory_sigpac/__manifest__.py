1d564a6288262bb6359da5084e35d44ac02ab2a0
Git Moval <git@moval.es>
2025-11-26 23:16:01 +0100

    [MIG] l10n_es_territory_sigpac: Migration to 17.0

--- l10n_es_territory_sigpac/__manifest__.py
+++ l10n_es_territory_sigpac/__manifest__.py
@@ -1,4 +1,3 @@
-# -*- coding: utf-8 -*-
 # 2025 Moval Agroingeniería
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
 
@@ -6,7 +5,7 @@
     "name": "Territory: SIGPAC Integration",
     "summary": "In a territorial census, integration of the SIGPAC "
                "enclosures, and creation of a spatial link with the parcels.",
-    "version": '16.0.1.0.0',
+    "version": "17.0.1.0.0",
     "category": "Territory Management",
     "website": "http://www.moval.es",
     "author": "Moval Agroingeniería",
