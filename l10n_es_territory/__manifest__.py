4f09c5a2bf1b2129c5ba741c703bd36fadeb44b4
Git Moval <git@moval.es>
2025-11-27 06:30:26 +0100

    [MIG] l10n_es_territory: Migration to 18.0

--- l10n_es_territory/__manifest__.py
+++ l10n_es_territory/__manifest__.py
@@ -5,7 +5,7 @@
     'name': 'Base-Territory (Spanish Localization)',
     'summary': 'Customization of the territorial base (base_ter module) '
                'to the administrative scope of Spain.',
-    'version': "17.0.1.0.0",
+    'version': "18.0.1.0.0",
     'author': 'Moval Agroingeniería',
     'license': 'AGPL-3',
     'website': 'https://moval.es',
