be9829f9de5d3436735a6d55a08d97bdfa3045a9
Git Moval <git@moval.es>
2025-11-27 06:30:27 +0100

    [MIG] base_ter_invoicing: Migration to 18.0

--- base_ter_invoicing/__manifest__.py
+++ base_ter_invoicing/__manifest__.py
@@ -5,7 +5,7 @@
     'name': 'Area-based massive invoicing',
     'summary': 'Extension of the massive invoicing module (base_invoicing) '
                'to support area-based billing.',
-    'version': "17.0.1.0.0",
+    'version': "18.0.1.0.0",
     'category': 'Accounting/Accounting',
     'website': 'https://www.moval.es',
     'author': 'Moval Agroingeniería',
