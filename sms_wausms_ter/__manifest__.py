e33d281d28737251b5e819bf856fd5e5c02fd819
César Andrés Sánchez <rasec.andres@gmail.com>
2026-01-02 05:18:23 +0100

    [UPD] sms_wausms_ter utf-8 flake

--- sms_wausms_ter/__manifest__.py
+++ sms_wausms_ter/__manifest__.py
@@ -2,21 +2,21 @@
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 
 {
-    'name': 'SMS WauSMS Territory',
-    'summary': 'SMS Text Messaging for Territory',
-    'version': "18.0.1.0.0",
-    'category': 'Tools',
-    'website': 'https://www.moval.es',
-    'author': 'Moval Agroingeniería',
-    'license': 'AGPL-3',
-    'application': False,
-    'installable': True,
-    'depends': [
-        'base_ter',
-        'sms_wausms',
+    "name": "SMS WauSMS Territory",
+    "summary": "SMS Text Messaging for Territory",
+    "version": "18.0.1.0.0",
+    "category": "Tools",
+    "website": "https://www.moval.es",
+    "author": "Moval Agroingeniería",
+    "license": "AGPL-3",
+    "application": False,
+    "installable": True,
+    "depends": [
+        "base_ter",
+        "sms_wausms",
     ],
-    'data_old': [
-        'views/ter_parcel_views.xml',
-        'views/ter_property_views.xml',
+    "data_old": [
+        "views/ter_parcel_views.xml",
+        "views/ter_property_views.xml",
     ],
 }
