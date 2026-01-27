5c60da2d9d74d366f1b91e4196df51290c164a25
César Andrés Sánchez <rasec.andres@gmail.com>
2026-01-27 19:04:55 +0100

    [REF] <module>: adapt hooks signature and normalize SQL for Odoo 18
    - Use Odoo 18 env-based hook signatures
    - Keep post-init data fix for partnerlink overhead defaults
    - Keep uninstall data reset for category_code=2

--- base_ter_invoicing/hooks.py
+++ base_ter_invoicing/hooks.py
@@ -1,28 +1,34 @@
-# 2025 Moval Agroingeniería
-# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html)
+# Copyright 2025 Moval Agroingeniería
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl.html)
 
-from odoo import SUPERUSER_ID, api, exceptions
+from odoo import SUPERUSER_ID, api
 
 
-def post_init_hook(cr, registry):
-    env = api.Environment(cr, SUPERUSER_ID, {})
+def post_init_hook(env):
+    if not isinstance(env, api.Environment):
+        env = api.Environment(env, SUPERUSER_ID, {})
+
     env.cr.execute(
         """
-        UPDATE ter_parcel_partnerlink tpp
-        SET percentage_overhead = 100,
-        area_overhead = tp.area_official
-        FROM ter_parcel tp
-        WHERE tpp.parcel_id = tp.id AND is_main
-    """
+        UPDATE ter_parcel_partnerlink AS tpp
+        SET
+            percentage_overhead = 100.0,
+            area_overhead = tp.area_official
+        FROM ter_parcel AS tp
+        WHERE tpp.parcel_id = tp.id
+          AND tpp.is_main IS TRUE
+        """
     )
 
 
-def uninstall_hook(cr, registry):
-    # Initialize the code of category 2 to 0 so that it can be deleted later.
-    env = api.Environment(cr, SUPERUSER_ID, {})
+def uninstall_hook(env):
+    if not isinstance(env, api.Environment):
+        env = api.Environment(env, SUPERUSER_ID, {})
+
     env.cr.execute(
         """
-        UPDATE product_category SET category_code = 0
+        UPDATE product_category
+        SET category_code = 0
         WHERE category_code = 2
-    """
+        """
     )
