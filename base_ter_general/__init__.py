27e6955ca311ab27f24f8bc83bb7419c9cd3139c
César Andrés Sánchez <rasec.andres@gmail.com>
2026-01-27 18:17:02 +0100

    [MIG] base_ter_general: clean settings field for Odoo 18
    - Remove obsolete field label on config_parameter-backed setting

--- base_ter_general/__init__.py
+++ base_ter_general/__init__.py
@@ -1,4 +1,4 @@
-# 2025 Moval Agroingeniería
+# 2025 - 2026 Moval Agroingeniería
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html)
 
 from . import models
--- base_ter_general/models/res_config_settings.py
+++ base_ter_general/models/res_config_settings.py
@@ -1,4 +1,4 @@
-# 2024 Moval Agroingeniería
+# 2024-2026 Moval Agroingeniería
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html)
 
 from odoo import fields, models
@@ -8,6 +8,5 @@ class ResConfigSettings(models.TransientModel):
     _inherit = "res.config.settings"
 
     jinja2_template = fields.Char(
-        string="Jinja2 Template",
         config_parameter="base_ter_general.jinja2_template",
     )
