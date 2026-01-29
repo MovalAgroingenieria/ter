584c01d1942ad351d9e454b5364e732adaab139f
César Andrés Sánchez <rasec.andres@gmail.com>
2026-01-29 03:59:59 +0100

    [REF] l10n_es_territory: clean compute extension for cadastral area
    Avoid returning values in compute methods and add explicit depends for
    cadastral area in GIS data.

--- l10n_es_territory/models/ter_gis_parcel_model.py
+++ l10n_es_territory/models/ter_gis_parcel_model.py
@@ -1,22 +1,24 @@
-# 2024-2026 Moval Agroingeniería
+# Copyright 2024-2026 Moval Agroingeniería
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html)
 
-from odoo import models
+from odoo import api, models
 
 
 class TerGisParcelModel(models.Model):
     _inherit = "ter.gis.parcel.model"
 
+    @api.depends("parcel_id.cadastral_area")
     def _compute_gis_data(self):
-        result = super()._compute_gis_data()
+        super()._compute_gis_data()
         formatter = self.env["common.format"]
         label = self.env._("Cadastral Area (m²)")
+
         for record in self:
             cadastral_area = record.parcel_id.cadastral_area if record.parcel_id else 0
             formatted_area = formatter.transform_integer_to_locale(cadastral_area or 0)
-            record.gis_data = (record.gis_data or "") + self.env._(
+            extra = self.env._(
                 "\n⸰ %(label)s: %(value)s",
                 label=label,
                 value=formatted_area,
             )
-        return result
+            record.gis_data = (record.gis_data or "") + extra
