b4dc86b4d7bbaf04c53a48adc01e8f816fbd374f
César Andrés Sánchez <rasec.andres@gmail.com>
2026-02-04 03:52:16 +0100

    [IMP] base_terr: adding default sequence

--- base_ter/__manifest__.py
+++ base_ter/__manifest__.py
@@ -28,6 +28,7 @@
         "security/security.xml",
         "security/ir.model.access.csv",
         "data/ter_profile_data.xml",
+        "data/ter_unit_sequence_data.xml",
         "data/base_ter_cron.xml",
         "wizards/wizard_import_kml_views.xml",
         "wizards/wizard_set_parcel_code_views.xml",
