365a3626609c884b12720abc2f574142034f0307
César Andrés Sánchez <rasec.andres@gmail.com>
2026-02-04 06:04:28 +0100

    [IMP] base_ter: Updating migration scripts

---
+++ l10n_es_territory/migrations/18.0.1.0.1/post-migration.py
@@ -0,0 +1,45 @@
+# Copyright 2024-2026 Moval Agroingeniería S.L.
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl.html)
+
+"""Add mapped_to_polygon column to base_adi tables if missing.
+
+res.admregion, res.province and res.municipality inherit polygon.model which
+defines mapped_to_polygon (store=True). If base_adi was installed before
+polygon.model had this field stored, or the DB was restored without the column,
+this migration adds it.
+"""
+
+import logging
+
+_logger = logging.getLogger(__name__)
+
+
+def migrate(cr, version):
+    if not version:
+        return
+
+    for table in ("res_admregion", "res_province", "res_municipality"):
+        try:
+            cr.execute(
+                """
+                SELECT column_name
+                FROM information_schema.columns
+                WHERE table_name = %s AND column_name = 'mapped_to_polygon'
+                """,
+                (table,),
+            )
+            if cr.fetchone():
+                continue
+            cr.execute(
+                'ALTER TABLE "%s" ADD COLUMN mapped_to_polygon BOOLEAN DEFAULT FALSE'
+                % table
+            )
+            _logger.info(
+                "l10n_es_territory: Added mapped_to_polygon column to %s", table
+            )
+        except Exception as e:
+            _logger.warning(
+                "l10n_es_territory: Could not add mapped_to_polygon to %s: %s",
+                table,
+                e,
+            )
