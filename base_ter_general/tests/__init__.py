e00b802d4ebcb968456b7e55816af117815e3fcd
César Andrés Sánchez <candres@moval.es>
2026-02-24 16:37:41 +0100

    [ADD] base_ter github token

--- base_ter_general/tests/__init__.py
+++ base_ter_general/tests/__init__.py
@@ -1,1 +1,3 @@
+# pylint: disable=import-self
+
 from . import test_res_partner_mapping, test_ter_parcel
--- ter_analytic_geofolia/tests/__init__.py
+++ ter_analytic_geofolia/tests/__init__.py
@@ -1,4 +1,5 @@
 # Copyright 2026 Moval Agroingeniería S.L.
 # License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl.html)
+# pylint: disable=import-self
 
 from . import test_import_job, test_import_wizard
