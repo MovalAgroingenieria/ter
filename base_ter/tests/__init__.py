4fa715f59ce34d8618e791718d0ef1ce885e9d96
César Andrés Sánchez <candres@moval.es>
2026-02-24 16:35:25 +0100

    [ADD] base_ter disable import

--- base_ter/tests/__init__.py
+++ base_ter/tests/__init__.py
@@ -1,5 +1,6 @@
 # Copyright 2026 Moval Agroingeniería S.L.
 # License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl.html)
+# pylint: disable=import-self
 
 from . import (
     gis_viewer_test_model,
