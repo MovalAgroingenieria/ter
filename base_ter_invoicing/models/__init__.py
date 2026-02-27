2da54d9fc7810900a86166a6f973e7cdfd1b985c
candres-moval <candres@moval.es>
2026-02-27 12:52:53 +0100

    Revert "[ADD] base_ter_invoicing: pep8-flake and tests"

--- base_ter_invoicing/models/__init__.py
+++ base_ter_invoicing/models/__init__.py
@@ -1,4 +1,4 @@
-# 2026 Moval Agroingeniería
-# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl.html)
+# 2025-2026 Moval Agroingeniería
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html)
 
 from . import account_move_line, ter_parcel
